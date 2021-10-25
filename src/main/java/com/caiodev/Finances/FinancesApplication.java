4c40b31472b899aa0cc4b3794befb72f4bc46f9b
Caio Victor Batista de Macedo <79578694+ccaiomacedo@users.noreply.github.com>
2021-10-25 09:50:41 -0300

    Update FinancesApplication.java

--- src/main/java/com/caiodev/Finances/FinancesApplication.java
+++ src/main/java/com/caiodev/Finances/FinancesApplication.java
@@ -9,7 +9,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 @EnableWebMvc // permitindo que aplicações de fora tenham acesso
 public class FinancesApplication implements WebMvcConfigurer {
-
+	
+	/*CORS é um mecanismo que permite que recursos restritos em uma página da web sejam recuperados por outro domínio 
+	fora do domínio ao qual pertence o recurso que será recuperado*/
 	@Override
 	public void addCorsMappings(CorsRegistry registry){
 		registry.addMapping("/**").allowedMethods("GET","POST","PUT","DELETE","OPTIONS");
